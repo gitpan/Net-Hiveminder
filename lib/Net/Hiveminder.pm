2570f37e01d16c881c90f97fb01b58177c0a3fbe
Jesse Vincent <jesse+cpan@fsck.com>
2009-04-20 07:32:48 +0000

    Import of JESSE/Net-Hiveminder-0.07 from CPAN.
    gitpan-cpan-distribution: Net-Hiveminder
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         JESSE/Net-Hiveminder-0.07.tar.gz
    gitpan-cpan-author:       JESSE
    gitpan-cpan-maturity:     released

--- lib/Net/Hiveminder.pm
+++ lib/Net/Hiveminder.pm
@@ -3,39 +3,11 @@ package Net::Hiveminder;
 use Moose;
 extends 'Net::Jifty';
 
+our $VERSION = '0.07';
+
+use DateTime;
 use Number::RecordLocator;
 my $LOCATOR = Number::RecordLocator->new;
-
-=head1 NAME
-
-Net::Hiveminder - Perl interface to hiveminder.com
-
-=head1 VERSION
-
-Version 0.05 released 17 Mar 08
-
-=cut
-
-our $VERSION = '0.05';
-
-=head1 SYNOPSIS
-
-    use Net::Hiveminder;
-    my $hm = Net::Hiveminder->new(use_config => 1);
-    print $hm->todo;
-    $hm->create_task("Boy these pretzels are making me thirsty [due tomorrow]");
-
-=head1 DESCRIPTION
-
-Hiveminder is a collaborate todo list organizer, built with L<Jifty>.
-
-This module uses Hiveminder's REST API to let you manage your tasks any way you
-want to.
-
-This module is built on top of L<Net::Jifty>. Consult that module's
-documentation for the lower-level interface.
-
-=cut
 
 has '+site' => (
     default => 'http://hiveminder.com'
@@ -56,32 +28,6 @@ has '+config_file' => (
 has '+filter_file' => (
     default => ".hm",
 );
-
-=head2 display_tasks [ARGS], TASKS
-
-This will take a list of hash references, C<TASKS>, and convert each to a
-human-readable form.
-
-In scalar context it will return the readable forms of these tasks joined by
-newlines.
-
-Passing options into this is somewhat tricky, because tasks are currently
-regular hashes. You may pass arguments to this method as such:
-
-    $hm->display_tasks([arg1 => 'val1', arg2 => 'val2'], @tasks)
-
-The arguments currently respected are:
-
-=over 4
-
-=item linkify_locator
-
-Make the record locator (C<#foo>) into an HTML link, pointing to the task on
-C<site>.
-
-=back
-
-=cut
 
 sub display_tasks {
     my $self = shift;
@@ -153,31 +99,18 @@ sub display_tasks {
     return wantarray ? @out : join "\n", @out;
 }
 
-=head2 get_tasks ARGS
-
-Runs a search with C<ARGS> for tasks. There are no defaults here, so this can
-be used for anything.
-
-Returns a list of hash references, each one being a task. Use C<display_tasks>
-if necessary.
-
-=cut
-
 sub get_tasks {
     my $self = shift;
-    return @{ $self->act('TaskSearch', @_)->{content}{tasks} };
+    my @args = @_;
+    unshift @args, "tokens" if @args == 1;
+
+    return @{ $self->act('TaskSearch', @args)->{content}{tasks} };
 }
-
-=head2 todo_tasks [ARGS]
-
-Returns a list of hash references, each one a task. This uses the same query
-that the home page of Hiveminder uses. The optional C<ARGS> will be passed as
-well so you can narrow down your todo list.
-
-=cut
 
 sub todo_tasks {
     my $self = shift;
+    my @args = @_;
+    unshift @args, "tokens" if @args == 1;
 
     $self->get_tasks(
         complete_not     => 1,
@@ -190,31 +123,17 @@ sub todo_tasks {
 
         # XXX: this is one place to improve the API
 
-        @_
+        @args
     );
 }
 
-=head2 todo [ARGS]
-
-Returns a list of tasks in human-readable form. The optional C<ARGS> will be
-passed as well so you can narrow down your todo list.
-
-In scalar context it will return the concatenation of the tasks.
-
-=cut
-
 sub todo {
     my $self = shift;
+    my $opts = [];
+    $opts = shift if ref($_[0]) eq 'ARRAY';
 
-    $self->display_tasks( $self->todo_tasks(@_) );
+    $self->display_tasks( $opts, $self->todo_tasks(@_) );
 }
-
-=head2 create_task SUMMARY, ARGS
-
-Creates a new task with C<SUMMARY>. You may also specify arguments such as what
-tags the task will have.
-
-=cut
 
 sub create_task {
     my $self    = shift;
@@ -228,12 +147,6 @@ sub create_task {
     );
 }
 
-=head2 read_task LOCATOR
-
-Load task C<LOCATOR>.
-
-=cut
-
 sub read_task {
     my $self  = shift;
     my $loc   = shift;
@@ -241,12 +154,6 @@ sub read_task {
 
     return $self->read(Task => id => $id);
 }
-
-=head2 update_task LOCATOR, ARGS
-
-Update task C<LOCATOR> with C<ARGS>.
-
-=cut
 
 sub update_task {
     my $self = shift;
@@ -256,12 +163,6 @@ sub update_task {
     return $self->update(Task => id => $id, @_);
 }
 
-=head2 delete_task LOCATOR
-
-Delete task C<LOCATOR>.
-
-=cut
-
 sub delete_task {
     my $self = shift;
     my $loc  = shift;
@@ -269,26 +170,6 @@ sub delete_task {
 
     return $self->delete(Task => id => $id);
 }
-
-=head2 bulk_update ARGS
-
-Bulk-updates the given tasks. You can pass tasks in with one or more of the
-following:
-
-=over 4
-
-=item tasks
-
-An array reference of task hashes or locators, or a space-delimited string of
-locators.
-
-=item ids
-
-An array reference or space-delimited string of task IDs.
-
-=back
-
-=cut
 
 sub bulk_update {
     my $self = shift;
@@ -321,12 +202,6 @@ sub bulk_update {
     );
 }
 
-=head2 complete_tasks TASKS
-
-Marks the list of tasks or locators as complete.
-
-=cut
-
 sub complete_tasks {
     my $self = shift;
     $self->bulk_update(
@@ -334,28 +209,6 @@ sub complete_tasks {
         complete => 1,
     );
 }
-
-=head2 braindump TEXT[, ARGS]
-
-Braindumps C<TEXT>.
-
-Optional arguments:
-
-=over 4
-
-=item tokens => string | arrayref
-
-tokens may be used to provide default attributes to all the braindumped tasks
-(this is part of what the filter feature of Hiveminder's IM bot does).
-
-=item returns => 'ids' | 'tasks'
-
-Return the affected task IDs, or the tasks themselves, instead of a summary of
-the changes made.
-
-=back
-
-=cut
 
 sub braindump {
     my $self = shift;
@@ -390,12 +243,6 @@ sub braindump {
     return $ret->{message};
 }
 
-=head2 upload_text TEXT
-
-Uploads C<TEXT> to BTDT::Action::UploadTasks.
-
-=cut
-
 sub upload_text {
     my $self = shift;
     my $text = shift;
@@ -403,12 +250,6 @@ sub upload_text {
     return $self->act(UploadTasks => content => $text, format => 'sync')
                 ->{message};
 }
-
-=head2 upload_file FILENAME
-
-Uploads C<FILENAME> to BTDT::Action::UploadTasks.
-
-=cut
 
 sub upload_file {
     my $self = shift;
@@ -418,17 +259,6 @@ sub upload_file {
 
     return $self->upload_text($text);
 }
-
-=head2 download_text
-
-Downloads your tasks. This also gets the metadata so that you can edit the text
-and upload it, and it'll make the same changes to your task list.
-
-This does not currently accept query arguments, because Hiveminder expects a
-"/not/owner/me/group/personal" type argument string, when all we can produce is
-"owner_not => 'me', group => 'personal'"
-
-=cut
 
 sub download_text {
     my $self = shift;
@@ -440,16 +270,6 @@ sub download_text {
     )->{content}{result};
 }
 
-=head2 download_file FILENAME
-
-Downloads your tasks and puts them into C<FILENAME>.
-
-This does not currently accept query arguments, because Hiveminder expects a
-"/not/owner/me/group/personal" type argument string, when all we can produce is
-"owner_not => 'me', group => 'personal'"
-
-=cut
-
 sub download_file {
     my $self = shift;
     my $file = shift;
@@ -460,13 +280,6 @@ sub download_file {
     print $handle $text;
     close $handle;
 }
-
-=head2 priority (NUMBER | TASK) -> Maybe String
-
-Returns the "word" of a priority. One of: lowest, low, normal, high, highest.
-If the priority is out of range, C<undef> will be returned.
-
-=cut
 
 my @priorities = (undef, qw/lowest low normal high highest/);
 sub priority {
@@ -480,12 +293,6 @@ sub priority {
     return $priorities[$priority];
 }
 
-=head2 done LOCATORS
-
-Marks the given tasks as complete.
-
-=cut
-
 sub done {
     my $self = shift;
 
@@ -494,12 +301,6 @@ sub done {
         $self->update('Task', id => $id, complete => 1);
     }
 }
-
-=head2 loc2id (LOCATOR|TASK)s -> IDs
-
-Transforms the given record locators (or tasks) to regular IDs.
-
-=cut
 
 sub loc2id {
     my $self = shift;
@@ -519,16 +320,16 @@ sub loc2id {
     return wantarray ? @ids : $ids[0];
 }
 
+=head2 tasks2ids
+
+Deprecated 
+
+=cut
+
 sub tasks2ids {
     Carp::carp "Net::Hiveminder->tasks2ids is deprecated, use loc2id instead.";
     loc2id(@_);
 }
-
-=head2 id2loc IDs -> LOCATORs
-
-Transform the given IDs into record locators.
-
-=cut
 
 sub id2loc {
     my $self = shift;
@@ -538,12 +339,6 @@ sub id2loc {
     return wantarray ? @locs : $locs[0];
 }
 
-=head2 comments_on TASK -> (String)s
-
-Returns a list of the comments on the given task.
-
-=cut
-
 sub comments_on {
     my $self = shift;
     my $task = $self->loc2id(shift);
@@ -552,15 +347,6 @@ sub comments_on {
            map { $_->{message} }
            @{ $self->search('TaskEmail', task_id => $task) || [] };
 }
-
-=head2 comment_on TASK, MESSAGE
-
-Add a comment to TASK.
-
-This method requires L<Email::Simple::Creator>, which is an optional dependency
-of Net::Hiveminder. If Creator is unavailable, then this will throw an error.
-
-=cut
 
 sub comment_on {
     my $self = shift;
@@ -583,9 +369,212 @@ sub comment_on {
     );
 }
 
+sub send_feedback {
+    my $self = shift;
+    my $text = shift;
+
+    $self->act('SendFeedback', content => $text);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::Hiveminder - Perl interface to hiveminder.com
+
+=head1 SYNOPSIS
+
+    use Net::Hiveminder;
+    my $hm = Net::Hiveminder->new(use_config => 1);
+    print $hm->todo;
+    $hm->create_task("Boy these pretzels are making me thirsty [due tomorrow]");
+
+=head1 DESCRIPTION
+
+Hiveminder is a collaborate todo list organizer, built with L<Jifty>.
+
+This module uses Hiveminder's REST API to let you manage your tasks any way you
+want to.
+
+This module is built on top of L<Net::Jifty>. Consult that module's
+documentation for the lower-level interface.
+
+=head2 display_tasks [ARGS], TASKS
+
+This will take a list of hash references, C<TASKS>, and convert each to a
+human-readable form.
+
+In scalar context it will return the readable forms of these tasks joined by
+newlines.
+
+Passing options into this is somewhat tricky, because tasks are currently
+regular hashes. You may pass arguments to this method as such:
+
+    $hm->display_tasks([arg1 => 'val1', arg2 => 'val2'], @tasks)
+
+The arguments currently respected are:
+
+=over 4
+
+=item linkify_locator
+
+Make the record locator (C<#foo>) into an HTML link, pointing to the task on
+C<site>.
+
+=back
+
+=head2 get_tasks ARGS
+
+Runs a search with C<ARGS> for tasks. There are no defaults here, so this can
+be used for anything.
+
+Returns a list of hash references, each one being a task. Use C<display_tasks>
+if necessary.
+
+=head2 todo_tasks [ARGS]
+
+Returns a list of hash references, each one a task. This uses the same query
+that the home page of Hiveminder uses. The optional C<ARGS> will be passed as
+well so you can narrow down your todo list.
+
+=head2 todo [ARGS]
+
+Returns a list of tasks in human-readable form. The optional C<ARGS> will be
+passed as well so you can narrow down your todo list.
+
+In scalar context it will return the concatenation of the tasks.
+
+If the first argument is an array reference, it will be passed to
+L</display_tasks> as options.
+
+For example, to display tasks due today (with color):
+
+    print scalar $hm->todo([color => 1], due => "today");
+
+=head2 create_task SUMMARY, ARGS
+
+Creates a new task with C<SUMMARY>. You may also specify arguments such as what
+tags the task will have.
+
+=head2 read_task LOCATOR
+
+Load task C<LOCATOR>.
+
+=head2 update_task LOCATOR, ARGS
+
+Update task C<LOCATOR> with C<ARGS>.
+
+=head2 delete_task LOCATOR
+
+Delete task C<LOCATOR>.
+
+=head2 bulk_update ARGS
+
+Bulk-updates the given tasks. You can pass tasks in with one or more of the
+following:
+
+=over 4
+
+=item tasks
+
+An array reference of task hashes or locators, or a space-delimited string of
+locators.
+
+=item ids
+
+An array reference or space-delimited string of task IDs.
+
+=back
+
+=head2 complete_tasks TASKS
+
+Marks the list of tasks or locators as complete.
+
+=head2 braindump TEXT[, ARGS]
+
+Braindumps C<TEXT>.
+
+Optional arguments:
+
+=over 4
+
+=item tokens => string | arrayref
+
+tokens may be used to provide default attributes to all the braindumped tasks
+(this is part of what the filter feature of Hiveminder's IM bot does).
+
+=item returns => 'ids' | 'tasks'
+
+Return the affected task IDs, or the tasks themselves, instead of a summary of
+the changes made.
+
+=back
+
+=head2 upload_text TEXT
+
+Uploads C<TEXT> to BTDT::Action::UploadTasks.
+
+=head2 upload_file FILENAME
+
+Uploads C<FILENAME> to BTDT::Action::UploadTasks.
+
+=head2 download_text
+
+Downloads your tasks. This also gets the metadata so that you can edit the text
+and upload it, and it'll make the same changes to your task list.
+
+This does not currently accept query arguments, because Hiveminder expects a
+"/not/owner/me/group/personal" type argument string, when all we can produce is
+"owner_not => 'me', group => 'personal'"
+
+=head2 download_file FILENAME
+
+Downloads your tasks and puts them into C<FILENAME>.
+
+This does not currently accept query arguments, because Hiveminder expects a
+"/not/owner/me/group/personal" type argument string, when all we can produce is
+"owner_not => 'me', group => 'personal'"
+
+=head2 priority (NUMBER | TASK) -> Maybe String
+
+Returns the "word" of a priority. One of: lowest, low, normal, high, highest.
+If the priority is out of range, C<undef> will be returned.
+
+=head2 done LOCATORS
+
+Marks the given tasks as complete.
+
+=head2 loc2id (LOCATOR|TASK)s -> IDs
+
+Transforms the given record locators (or tasks) to regular IDs.
+
+=head2 id2loc IDs -> LOCATORs
+
+Transform the given IDs into record locators.
+
+=head2 comments_on TASK -> (String)s
+
+Returns a list of the comments on the given task.
+
+=head2 comment_on TASK, MESSAGE
+
+Add a comment to TASK.
+
+This method requires L<Email::Simple::Creator>, which is an optional dependency
+of Net::Hiveminder. If Creator is unavailable, then this will throw an error.
+
+=head2 send_feedback TEXT
+
+Sends the given TEXT as feedback to the Hiveminder team.
+
 =head1 SEE ALSO
 
-L<Jifty>, L<Net::Jifty>
+L<App::Todo>, L<Jifty>, L<Net::Jifty>
 
 =head1 AUTHOR
 
@@ -599,12 +588,10 @@ L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Hiveminder>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Best Practical Solutions.
+Copyright 2007-2008 Best Practical Solutions.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
-
-1;
 
